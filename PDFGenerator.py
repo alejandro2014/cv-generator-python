6538633550b49081648ea382d1e5f7f553745689
Alejandro <alejandro@desktop>
2020-11-23 19:36:17 +0100

    Centered the logos

--- PDFGenerator.py
+++ PDFGenerator.py
@@ -118,9 +118,12 @@ class PDFGenerator(FPDF):
         self.current_region.add_height(height)
         self.draw_region(self.current_region)
 
+        logo_path = 'config/logos/' + experience['company']['id'] + '.png'
+        self.image(logo_path, self.current_region.mid_x - (42.0 - 5.0) / 2.54, self.current_y + 5.0)
+
+        self.add_line(4)
         experience_times = self.string_processor.get_experience_times(experience)
         self.write_string(experience_times, 'C')
-        self.image(experience['company']['logo'], self.current_region.mid_x - 41.0 / 2.54, self.current_y)
 
         self.current_y += height
         self.cursor_y += height
