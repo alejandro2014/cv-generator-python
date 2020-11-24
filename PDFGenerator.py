d795eaf3655aa464970b5460bd956071efb3050d
Alejandro <alejandro@desktop>
2020-11-24 19:27:59 +0100

    Able to draw the region

--- PDFGenerator.py
+++ PDFGenerator.py
@@ -21,7 +21,6 @@ class PDFGenerator(FPDF):
         self.string_processor = StringProcessor()
 
         self.cursor_y = 0.0
-        #self.current_y = 0.0
         self.step_no = 1
         self.company_name = company_name
         self.__line_drawer = LineDrawer(self)
@@ -67,7 +66,6 @@ class PDFGenerator(FPDF):
     def generate_cv(self, cv_data):
         self.add_page()
 
-        self.__line_drawer.position_line()
         #self.__line_drawer.draw_region(100.0)
 
         self.generate_header(cv_data['header'])
@@ -81,33 +79,23 @@ class PDFGenerator(FPDF):
     def generate_header(self, header):
         self.change_font('mainTitle')
         self.write_string_ln(header['name'], 'C')
-        self.__line_drawer.position_line()
 
         self.change_font('mainSubTitle')
         self.write_string_ln(header['position'], 'C')
-        self.__line_drawer.position_line()
 
         self.change_font('normalText')
         self.write_string_ln('Address: ' + header['address'], 'C')
-        self.__line_drawer.position_line()
-
         self.write_string_ln(header['mail'] + ', ' + header['phone'], 'C')
-        self.__line_drawer.position_line()
-
         self.write_string_ln('Place and date of birth: ' + header['birth']['date'] + ', ' + header['birth']['place'], 'C')
-        self.__line_drawer.position_line()
 
         self.add_line(2)
-        self.__line_drawer.position_line()
 
     def generate_profile(self, profile):
         self.change_font('sectionHeader')
         self.write_string_ln('Profile')
-        self.__line_drawer.position_line()
 
         self.change_font('normalText')
         self.write_paragraph(profile['introduction'])
-        self.__line_drawer.position_line()
 
     def generate_experiences(self, experiences):
         self.change_font('sectionHeader')
@@ -138,7 +126,6 @@ class PDFGenerator(FPDF):
 
     def generate_experience(self, experience):
         self.split_region('25%')
-        #self.__line_drawer.draw_region(100.0)
 
         self.change_font('normalText')
         self.change_region(1)
@@ -148,39 +135,33 @@ class PDFGenerator(FPDF):
         height = self.get_section_height(lines)
 
         self.current_region.set_height(height)
-        self.__line_drawer.draw_region_border()
         #self.__line_drawer.position_line()
         #self.__line_drawer.draw_region(100.0)
 
         #if not self.is_experience_fitting(height):
         #    self.add_page()
 
-        #    self.current_y = 10.0
-        #    current_y = self.current_y
-
-        #    self.set_region(region_right)
-
         #self.current_region.add_height(height)
         #self.draw_region(self.current_region)
 
         self.write_lines(lines)
+        self.__line_drawer.draw_region_border()
 
-        #self.current_y = current_y
-        return
+        #---------------------------------------------
+        #self.__line_drawer.draw_region(100.0)
+        self.change_region(0)
 
-        #self.set_region(region_left)
+        self.current_region.set_height(height)
+        #self.__line_drawer.draw_region(100.0)
 
-        #self.current_region.add_height(height)
-        #self.draw_region(self.current_region)
+        logo_path = 'config/logos/' + experience['company']['id'] + '.png'
+        self.image(logo_path, self.current_region.mx() - (42.0 - 5.0) / 2.54, self.current_region.cursor_y())
 
-        #logo_path = 'config/logos/' + experience['company']['id'] + '.png'
-        #self.image(logo_path, self.current_region.mid_x - (42.0 - 5.0) / 2.54, self.current_y + 5.0)
+        self.add_line(3)
+        experience_times = self.string_processor.get_experience_times(experience)
+        self.write_string(experience_times, 'C')
+        self.__line_drawer.draw_region_border()
 
-        ##self.add_line(4)
-        #experience_times = self.string_processor.get_experience_times(experience)
-        #self.write_string(experience_times, 'C')
-
-        #self.current_y += height
         #self.cursor_y += height
 
     def is_experience_fitting(self, height):
@@ -264,7 +245,6 @@ class PDFGenerator(FPDF):
         for i in range(1, lines_no + 1):
             offset_y = self.font['size'] / 2.54
             r.inc_y_cursor(offset_y)
-            #self.current_y += offset_y
 
     def change_font(self, font_name):
         font = copy.copy(self.pdf_fonts[font_name])
@@ -276,15 +256,7 @@ class PDFGenerator(FPDF):
 
         self.set_font(font['family'], font['style'], font['size'])
 
-        #self.current_region.inc_y_cursor(font['size'] / 2.54)
         self.font = font
-
-    def set_region(self, region):
-        region.process_y(self.current_y)
-        self.current_region = region
-
-        self.current_y = region.start_y_padded
-        self.cursor_y = self.current_y + self.font['size'] / 2.54
 
     def company_mark(self):
         self.change_font('smallText')
